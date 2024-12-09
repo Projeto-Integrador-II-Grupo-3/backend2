dcf16fa1a002d5911874e05f7fcac73ddf6f7468
João Bispo <joaovitor.bispo.1000@gmail.com>
2024-12-09 18:21:56 -0300

    security ok
    falta acrescentar o calculo do imc

--- src/main/java/com/generation/projetointegradorfit/repository/UsuarioRepository.java
+++ src/main/java/com/generation/projetointegradorfit/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.generation.projetointegradorfit.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +13,7 @@ import com.generation.projetointegradorfit.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+    
+    public Optional<Usuario> findByUsuario(@Param("usuario") String usuario);
 
 }
