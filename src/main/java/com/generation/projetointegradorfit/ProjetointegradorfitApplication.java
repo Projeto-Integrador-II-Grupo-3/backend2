1d5669b6368557b8b347b0c0cad6906570c5cdca
TiihAlves <tiih.alves1@gmail.com>
2024-12-09 11:22:39 -0300

    Inicio Projeto Integrador II

---
+++ src/main/java/com/generation/projetointegradorfit/ProjetointegradorfitApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegradorfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetointegradorfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetointegradorfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projetointegradorfit/ProjetointegradorfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegradorfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetointegradorfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
